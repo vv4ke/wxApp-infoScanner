c360e8b69a624ecf05098d4dbe47c686b462d8ae
zeng-jiahao <2577284412@qq.com>
2023-04-25 15:54:10 +0800

    更新多线程、写入excel

--- infoFinder.py
+++ infoFinder.py
@@ -1,6 +1,29 @@
 import re
 import os
 import yaml
+import queue
+import threading
+import pandas as pd
+
+
+# 定义一个线程执行的任务函数
+def worker(task_queue, results_queue):
+    while True:
+        try:
+            task_arg = task_queue.get(block=False)
+            reg_rule_name, regex, target_folder, file_scan_config = task_arg
+            match_result = match_content(regex, target_folder, file_scan_config)
+            # 将处理结果存储到 results 队列中
+            result = [reg_rule_name, match_result]
+            results_queue.put(result)
+            task_queue.task_done()
+        except queue.Empty:
+            # 捕获队列为空的异常
+            break
+        except Exception as e:
+            # 捕获其他异常
+            print(f"Caught an exception: {e}")
+            task_queue.task_done()
 
 
 def load_config(config_yaml_path=None):
@@ -14,17 +37,41 @@ def load_config(config_yaml_path=None):
         return config
 
 
-def start_scan(reg_config, target_folder):
+def start_scan(file_scan_config, regex_config, target_folder):
+    # 创建基本变量
     match_results = {}
-    for reg_rules in reg_config:
-        reg = reg_config[reg_rules]
-        match_result = match_content(reg, target_folder)
-        match_result = clear_list(match_result)
-        match_results[reg_rules] = match_result
+    task_queue = queue.Queue()
+    results_queue = queue.Queue()
+    num_threads = 5
+    threads = []
+
+    # 任务队列
+    for reg_rule_name in regex_config:
+        regex = regex_config[reg_rule_name]
+        task_arg = [reg_rule_name.split('_'), regex, target_folder, file_scan_config]
+        task_queue.put(task_arg)
+
+    # 创建线程池
+    for i in range(num_threads):
+        t = threading.Thread(target=worker, args=(task_queue, results_queue))
+        t.start()
+        threads.append(t)
+
+    # 阻塞直到所有任务完成
+    task_queue.join()
+
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+
+    # 将结果取出并存储到列表中
+    while not results_queue.empty():
+        reg_rule_name, match_result = results_queue.get(block=False)
+        match_results[reg_rule_name] = match_result
     return match_results
 
 
-def match_content(reg, target_folder):
+def match_content(reg, target_folder, file_scan_config):
     reg = re.compile(reg)
     match_results = []
     for (current_path, son_folders_name, files_name) in os.walk(target_folder):
@@ -32,21 +79,24 @@ def match_content(reg, target_folder):
         # 先匹配当前文件夹中的文件
         if files_name is not None:
             for file in files_name:
-                with open(os.path.join(current_path, file), 'r', encoding='utf-8') as f:
-                    file_content = f.read()
-                    match_result = reg.findall(file_content)
-                    match_result = clear_list(match_result)
-                    match_results += match_result
+                if check_suffix(file, file_scan_config):
+                    with open(os.path.join(current_path, file), 'r', encoding='utf-8') as f:
+                        file_content = f.read()
+                        match_result = reg.findall(file_content)
+                        # match_result = clear_list(match_result)
+                        match_results += match_result
+                else:
+                    continue
 
         # 递归匹配子文件夹中的文件
         if son_folders_name is not None:
             for son_folder_name in son_folders_name:
                 target_folder = os.path.join(current_path, son_folder_name)
                 match_result = match_content(reg, target_folder)
-                match_result = clear_list(match_result)
+                # match_result = clear_list(match_result)
                 match_results += match_result
 
-    return match_results
+    return clear_list(match_results)
 
 
 def clear_list(list01):
@@ -59,12 +109,41 @@ def clear_list(list01):
     return tamp_list
 
 
+def check_suffix(filename, file_scan_config):
+    filename_split = filename.split('.')
+    if file_scan_config['Black_Suffix_list']['active']:
+        if len(filename_split) > 1 and filename_split[-1] in file_scan_config['Black_Suffix_list']['suffix_list']:
+            return False
+        else:
+            return True
+    elif file_scan_config['White_Suffix_list']['active']:
+        if len(filename_split) > 1 and filename_split[-1] in file_scan_config['White_Suffix_list']['suffix_list']:
+            return True
+        else:
+            return False
+
+
+def write2excel(match_results):
+    df = pd.DataFrame.from_dict(match_results, orient='index').transpose()
+
+    # 将数据存储到Excel表格中
+    writer = pd.ExcelWriter('data.xlsx', engine='openpyxl')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.book.save('data.xlsx')
+    return
+
+
 def main():
     config = list(load_config(r'./config.yaml'))
+    file_scan_config = config[0]
+    regex_config = config[1]
+    requests_config = config[2]
     target_folder = r'./test_folder'
-    match_results = start_scan(config[0], target_folder)
+    match_results = start_scan(file_scan_config, regex_config, target_folder)
     for i in match_results:
         print(f'\033[0;32;40m{i}\033[0m: {match_results[i]}')
+
+    write2excel(match_results)
     return
 
 
--- test.py
+++ test.py
@@ -1,24 +1,10 @@
-import re
+import pandas as pd
 
-public_ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b(?<!10\.|172\.(?:1[6-9]|2\d|3[01])\.|192\.168\.)(?<!\b127\.0\.0\.1\b)(?!255\.255\.255\.255)\b'
+# 读取JSON数据
+json_data = {'name': ['John', 'Mary', 'Peter', '1'], 'age': [25, 30, 35]}
+df = pd.DataFrame.from_dict(json_data, orient='index').transpose()
 
-test_data = [
-    '192.168.0.1',
-    '10.0.0.1',
-    '172.16.0.1',
-    '172.31.255.255',
-    'http://192.168.0.1/a',
-    'The IP address is 10.10.10.10, which is a private IP address',
-    'Our local network uses the IP address 192.168.10.1',
-    'This IP address 172.20.1.1 is not valid',
-    'a10.0.0.1b',
-    '192.168.1.256',
-    '10.256.0.1',
-    'This is a public IP address: 216.58.194.174',
-    'Another public IP address is 151.101.1.69',
-    'The IP address 192.168.0.1 is private, but 8.8.8.8 is public'
-]
-
-for data in test_data:
-    matches = re.findall(public_ip_regex, data)
-    print(f"{data}: {matches}")
+# 将数据存储到Excel表格中
+writer = pd.ExcelWriter('data.xlsx', engine='openpyxl')
+df.to_excel(writer, sheet_name='Sheet1', index=False)
+writer.book.save('data.xlsx')
