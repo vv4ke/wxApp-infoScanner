8849dba0c27bf4fe78e4819f4677a105d68a6e1d
zjh <2577284412@qq.com>
2023-05-14 23:58:31 +0800

    完成url的拼接设计与代码实现，自动验活填坑ing

--- modle/active_request.py
+++ modle/active_request.py
@@ -4,19 +4,33 @@ api接口未授权扫描以及非 200 状态码绕过。
 """
 import queue
 import re
-
 import requests
 from urllib.parse import urlparse
 import socket
+import threading
 
 
-def req_work(task_queue, results_queue):
+def req_work(task_queue, results_queue, Request_Config=None):
     # 定义一个线程执行的任务函数
     while True:
         try:
-            task_arg = task_queue.get(block=False)
-            reg_rule_name, regex, target_folder, file_scan_config = task_arg
-            match_result = match_content(regex, target_folder, file_scan_config)
+            url = task_queue.get(block=False)
+            for http_method in Request_Config['http_methods']:
+                try:
+                    response = requests.request(method=http_method,
+                                                url=url,
+                                                cookies=Request_Config['cookies'],
+                                                headers=Request_Config['headers'],
+                                                params=Request_Config['params'],
+                                                data=Request_Config['data'],
+                                                json=Request_Config['json'],
+                                                allow_redirects=Request_Config['allow_redirects'],
+                                                verify=Request_Config['verify'],
+                                                timeout=Request_Config['timeout'],
+                                                proxies=Request_Config['proxies']
+                                                )
+                except:
+                    pass
             # 将处理结果存储到 results 队列中
             result = [reg_rule_name, match_result]
             results_queue.put(result)
@@ -31,32 +45,54 @@ def req_work(task_queue, results_queue):
 
 
 def scan_active(url_list=None, uri_list=None, Request_Config=None):
-
     # 黑白名单过滤一下
     if url_list is None:
         url_list = []
     # 先用黑白名单筛选一波url和uri
     url_list = filter_list(url_list, Request_Config['hostname_filter_rule'])
     url_list = filter_list(url_list, Request_Config['ip_filter_rule'])
-    uri_list = filter_list(uri_list, Request_Config['uri_filter_rule']).insert(0, '')
+    uri_list = filter_list(uri_list, Request_Config['uri_filter_rule'])
 
-    # 存活过滤
+    # 存活过滤，顺带进行Finger识别
     url_list = host_alive(url_list)
 
     task_queue = queue.Queue()
     results_queue = queue.Queue()
     num_threads = 20
     threads = []
+    scan_result = []
 
     if url_list:
         return None
+
+    # 填充任务队列
+    target_list = []
     for url in url_list:
         for uri in uri_list:
-            # 开始请求url拼凑
-            tamp = scrabbled_url(url, uri, Request_Config['scrabbled_rule'])
-            task_arg = []
+            # 根据拼接策略拼接url
+            target = url_target(url, uri)
+            if target not in target_list:
+                target_list.append(target)
+                task_queue.put(target)
 
-    return
+    # 创建线程池
+    for i in range(num_threads):
+        t = threading.Thread(target=req_work, args=(task_queue, results_queue, Request_Config))
+        t.start()
+        threads.append(t)
+
+    # 阻塞直到所有任务完成
+    task_queue.join()
+
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+
+    # 将结果取出并存储到列表中
+    while not results_queue.empty():
+        reg_rule_name, match_result = results_queue.get(block=False)
+        match_results[reg_rule_name] = match_result
+    return match_results
 
 
 # 根据黑白名单过滤一下数组
@@ -97,6 +133,8 @@ def host_alive(url_list=None):
             alive_url_list.append(url)
         else:
             if tcp_alive(domain, port):
+                # Finger识别
+                #
                 alive_host.append([domain, port])
                 alive_url_list.append(url)
             else:
@@ -137,5 +175,64 @@ def tcp_alive(host, port):
         return False
 
 
-def scrabbled_url(url, uri, scrabbled_rule):
-    # 关于尽可能去重的问题，写一个递归函数，递归比较 url的最后一个和uri的第一个 ，如果相同，继续比较第二个，直到不同，然后函数返回 0,1,2 等,0表示0个重复，反之.拼接时，按照0,1,2值来删除uri
+def url_target(url, uri):
+    """
+    :param url:
+    :param uri:
+    :param scrabbled_rule:
+    :return: uriList
+
+    针对url和uri的多种情况：
+    1.对于存在get参数的url，选择不拼接uri，直接返回即可。
+    2.要根据路由绕过策略选择拼接方式，例如shiro的绕过、spring的绕过、nginx代理的绕过策略，特殊头的绕过等。
+        2.1.shiro：https://www.freebuf.com/vuls/362341.html
+        2.2.spring：
+    3.为了减少发包数量，提高命中正确率，应该对比url中的uri和uri。合理的”去重“，例如 http://www.baidu.com/a/b/c /b/d，应该拼接为 http://www.baidu.com/a/b/d
+
+    """
+    if is_page(url):
+        return url
+    else:
+        return scrabbled_url(url, uri)
+
+
+def is_page(url=''):
+    if '?' in url:
+        return True
+    elif '.' in url.split('/')[-1]:
+        return True
+    else:
+        return False
+
+
+def scrabbled_url(url, uri):
+    urlList = url.split('/')
+
+    if len(urlList) == 3:  # url中的路径为空
+        return url + '/' + uri
+    elif len(urlList) == 4:  # url中的路径为 /
+        return url + uri
+    elif uri.startswith('.'):  # uri以.开头
+        if url.endswith('/'):
+            return url + uri
+        else:
+            return url + '/' + uri
+    else:
+        if not uri.startswith('/'):
+            uri = '/' + uri
+        uri_start = uri.split('/')[1]
+        url_path_list = urlList[3:]
+        for i in range(len(url_path_list)):
+            if url_path_list[i] == uri_start:
+                return '/'.join(urlList[0:3 + i]) + uri
+        return url + uri
+
+
+if __name__ == '__main__':
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc', 'qwe/zxc'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc/', 'qwe/zxc'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc', '/qwe/zxc'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc', '/qwe/zxc/'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc/', '/zxc/a/zxc/'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc/', './qwe/zxc'))
+    print(scrabbled_url('https://www.baidu.com/qwe/asd/zxc', '../../qwe/zxc'))
--- modle/config.py
+++ modle/config.py
@@ -16,14 +16,17 @@ def load_config(config_yaml_path=None, load_type=None):
 
 if __name__ == '__main__':
     # 加载配置文件，并且设置为全局参数
-    config_yaml = r'./config.yaml'
+    config_yaml = r'../config/config.yaml'
 
-    all_config = load_config(config_yaml)
-    globals().update(all_config)  # 设置为全局参数
-    print(all_config)
-
-    File_Config = load_config(config_yaml, load_type='File_Config')
-    print(File_Config)
-
-    Regex_Config = load_config(config_yaml, load_type='Regex_Config')
-    print(Regex_Config)
+    # all_config = load_config(config_yaml)
+    # globals().update(all_config)  # 设置为全局参数
+    # print(all_config)
+    #
+    # File_Config = load_config(config_yaml, load_type='File_Config')
+    # print(File_Config)
+    #
+    # Regex_Config = load_config(config_yaml, load_type='Regex_Config')
+    # print(Regex_Config)
+    Request_Config = load_config(config_yaml, load_type='Request_Config')
+    if not Request_Config['allow_redirects']:
+        print(1)
